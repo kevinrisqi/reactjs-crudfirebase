27d49b38335c489043627ff4072cfb828e3483ce
kevinrisqi22@gmail.com <kevinrisqi222>
2020-04-30 10:06:16 +0700

    Modify BlogPost.jsx

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiRYazMwY20wZwcz_2__O0T9ndb-QBmh0",
+    authDomain: "reacts-js-dcd03.firebaseapp.com",
+    databaseURL: "https://reacts-js-dcd03.firebaseio.com",
+    projectId: "reacts-js-dcd03",
+    storageBucket: "reacts-js-dcd03.appspot.com",
+    messagingSenderId: "506204376723",
+    appId: "1:506204376723:web:851dcff1b530c87cca0766",
+    measurementId: "G-ZN945R31MV"
+  };
+
+  export default firebaseConfig;
